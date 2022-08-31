b4541f0ff5bd11a50b4bed64cb9555631ec5a959
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-31 16:10:48 -0700

    Add code to set up HTTP Basic Auth security for admin feature. Tried different code finally found one that works. Turns out have to reset server in order to load updated env variables file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
+
+  # http_basic_authenticate_with :name => ENV["HTTP_BASIC_USER"], :password => ENV["HTTP_BASIC_PASSWORD"]
+
+  # http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"], if: -> { ENV['RAILS_ENV'] == 'production' }
+  # http_basic_authenticate_with name: "user", password: "pass", if: -> { ENV['RAILS_ENV'] != 'production' }
+
   protect_from_forgery with: :exception
 
   private
+  
+  # def authenticate
+  #   authenticate_or_request_with_http_basic do |username, password|
+  #     username == ENV["HTTP_BASIC_USER"] && password == ENV["HTTP_BASIC_PASSWORD"]
+  #   end
+  # end
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
