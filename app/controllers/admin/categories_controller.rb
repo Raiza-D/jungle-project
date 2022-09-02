822bbca7b4581f5efa33024290eda6b96f92bb7d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-01 19:15:01 -0700

    Add content for index method. Add method for 'new' which will create or add a new category to database.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,14 @@
 class Admin::CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
+
+
+  # Select count(quantity) FROM products GROUP BY category_id
+
+  def new
+    @category = Category.new
+  end
+
 end
