127c58fa0338e2aa4c382776352d96ce1b551216
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-02 18:17:13 -0700

    Add spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
+
   has_secure_password
+  
   validates :password_confirmation, presence: true
 
 end
