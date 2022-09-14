65795ca0b84c54bc472c339f5c5f697dda0da6be
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-13 17:38:20 -0700

    Use validate method on 'product' variables. Remove comments

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,16 +5,13 @@ RSpec.describe Product, type: :model do
   before do
     @category = Category.new(name: 'Climbers')
     @category.save
-    # Remove the bang operator here. Not needed. It's just a test.
-    # Bang operator MUTATES the object or variable that calls it.
-    # So be careful when using it. 
   end
 
   describe 'Validations' do
     it 'should save new product when all required fields are set' do
       @product = Product.new(name: 'White Rose', description: 'White roses have a profound, plain, and enigmatic side by their genuineness and their purity.', category: @category, quantity: 45, price: 3999)
 
-      @product.save
+      @product.validate
       
       expect(@product).to be_valid
     end
@@ -23,26 +20,14 @@ RSpec.describe Product, type: :model do
       @product = Product.new(name: nil, description: 'Fusce in blandit libero. Duis eget tincidunt lorem.', category: @category, quantity: 90, price: 2599)
 
       @product.validate
-      # Do validation first, then save (using bang operator)
-      # BEST practice is to use 'validate' method, instead of actually changing the database.
-
-      # Use product.validate for tests instead of product.save
-      # Update all tests here.
-
-      # No need to set fields being tested to have value of nil. Just remove it.
-      # Wording on Compass confusing for this tip.
-
-      # ActiveRecord contains the default error messages that are produced.
-      # These error messages can be customized.
 
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'should produce an error if price is blank' do
       @product = Product.new(name: 'True Jasmine', description: 'Suspendisse eleifend imperdiet tellus non consectetur.', category: @category, quantity: 60)
-      # if price: nil, why does my test fail?
 
-      @product.save
+      @product.validate
 
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
@@ -50,7 +35,7 @@ RSpec.describe Product, type: :model do
     it 'should produce an error if quantity is blank' do
       @product = Product.new(name: 'Chocolate Vine', description: 'Fusce in blandit libero', category: @category, quantity: nil, price: 2599)
 
-      @product.save
+      @product.validate
       
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
@@ -58,7 +43,7 @@ RSpec.describe Product, type: :model do
     it 'should produce an error if category is blank' do
       @product = Product.new(name: 'Downy Clematis', description: 'Donec vel commodo', category: nil, quantity: 100, price: 5999)
 
-      @product.save
+      @product.validate
 
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
