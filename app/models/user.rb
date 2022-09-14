84ffc2ba5b3d7947c44ad834ed91ec3f55276e10
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-13 21:25:27 -0700

    Add validation to set and check password length when new user created

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, case_sensitive: false
 
   has_secure_password
+
+  validates_length_of :password, :minimum => 8, :on => :create
   
   validates :password_confirmation, presence: true
 
