e428da934358acb0c50fcfe2e641318b40f845a3
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-13 18:14:51 -0700

    Add two tests - if all req fields valid. If password and password confirmation do not match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should create a new user if all required fields are set' do
+      @user = User.new(name: 'Waldo Smith', email: 'waldos@email.com', password: '123', password_confirmation: '123')
+
+      @user.validate
+
+      expect(@user).to be_valid
+    end
+
+    # Password and password confirmation do not match
+    it 'should produce an error if password and password confirmation do not match' do
+      @user = User.new(name: 'Alice Jones', email: 'alicej@email.com', password: 'abc123', password_confirmation: '123')
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+  end
 end
