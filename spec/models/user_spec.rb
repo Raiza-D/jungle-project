6f6a2ebf004f0d33d6460a3ab40179df0582f16a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-09-13 18:19:11 -0700

    Add test for name - cannot be blank

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
+    # When all required fields have values
     it 'should create a new user if all required fields are set' do
       @user = User.new(name: 'Waldo Smith', email: 'waldos@email.com', password: '123', password_confirmation: '123')
 
@@ -17,6 +18,15 @@ RSpec.describe User, type: :model do
       @user.validate
 
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    # Name cannot be blank
+    it 'should produce an error if name is blank' do
+      @user = User.new(email: 'atlasrain@email.com', password: '123', password_confirmation: '123')
+
+      @user.validate
+
+      expect(@user.errors.full_messages).to include("Name can't be blank")
     end
 
   end
